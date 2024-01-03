c5e274188ee1e7d89bd282a2146bb540a25eaa81
Ankush Hegde <59443849+ankush07261@users.noreply.github.com>
2024-01-03 15:59:20 +0530

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,35 +2,34 @@ import React, { useState } from 'react'
 import '../css/experience.css';
 import '../css/mobileBtns.css';
 import { Link } from 'react-router-dom';
-import { demoprojects} from '../constants/Experience';
+import { demoprojects, projects} from '../constants/Experience';
 function MyWork() {
 
   const [demo, setDemo] = useState(demoprojects);
-  // const [project, setProject] = useState(projects);
+  const [project, setProject] = useState(projects);
 
   return (
     <>
       <div className="myWork">
         <br/>
         <Link to='/contact' className='mobile-nav-elements'>Contact me &rArr;</Link>
-      {/* <h2>Projects I have worked on: </h2>
+      <h2>Projects I have worked on: </h2>
       <br/>
       <div className="projects">
         {
           project.map((project,index) => (
             <div className="card" key={index}>
-          <img src={project.image} alt=''/>
+          <img src={project.image} alt='project pic'/>
           <p>
                 <h3>{project.title}</h3><br />
             <br />
                 <p>{ project.description}</p>
                 <a href={ project.liveURL} className="try-it" target="_blank" rel="noreferrer">Open</a>
-            <a className='github-link' href={ demo.gitURL} target="_blank" rel="noopener noreferrer">Github</a>
           </p>
         </div>
           ))
         }
-      </div> */}
+      </div>
 
       <h2>Some Demo Projects ...</h2>
       <br/>
