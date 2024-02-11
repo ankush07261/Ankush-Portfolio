ed29b4784dc091213f488fcaa6f0cb1fab93030f
Ankush Hegde <59443849+ankush07261@users.noreply.github.com>
2024-02-11 14:29:27 +0530

    Update Skills.js

--- src/constants/Skills.js
+++ src/constants/Skills.js
@@ -1,68 +1,70 @@
 export const webDev = [
-    {
-        title: "MongoDB"
-    },
-    {
-        title: "Javascript"
-    },
-    {
-        title: "EJS"
-    },
-    {
-        title: "ExpressJS"
-    },
-    {
-        title: "ReactJS"
-    },
-    {
-        title: "Next.JS"
-    },
-    {
-        title: "NodeJS"
-    },
-    {
-        title: "HTML"
-    },
-    {
-        title: "SQL"
-    },
-    {
-        title: "CSS / Bootstrap"
-    },
-    
-]
+  {
+    title: "MongoDB",
+  },
+  {
+    title: "Javascript",
+  },
+  {
+    title: "EJS",
+  },
+  {
+    title: "ExpressJS",
+  },
+  {
+    title: "ReactJS",
+  },
+  {
+    title: "Next.JS",
+  },
+  {
+    title: "NodeJS",
+  },
+  {
+    title: "HTML",
+  },
+  {
+    title: "SQL",
+  },
+  {
+    title: "CSS",
+  },
+  {
+    title: "Bootstrap",
+  },
+  {
+    title: "Webflow",
+  },
+];
 
 export const cyberSecurity = [
-    {
-        title: "NMAP"
-    },
-    {
-        title: "Nessus"
-    },
-    {
-        title: "Wireshark"
-    },
-    {
-        title: "Firewall"
-    }
-]
+  {
+    title: "NMAP",
+  },
+  {
+    title: "Nessus",
+  },
+  {
+    title: "Wireshark",
+  },
+];
 export const others = [
-    {
-        title: "Python"
-    },
-    {
-        title: "Java"
-    },
-    {
-        title: "C++"
-    },
-    {
-        title: "C"
-    },
-    {
-        title: "C#"
-    },
-    {
-        title: "UNIX"
-    }
-]
+  {
+    title: "Python",
+  },
+  {
+    title: "Java",
+  },
+  {
+    title: "C++",
+  },
+  {
+    title: "C",
+  },
+  {
+    title: "C#",
+  },
+  {
+    title: "UNIX",
+  },
+];
